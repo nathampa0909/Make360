bf7d1399052cc0a24b51e7d830ce0b3cbf09befe
BuildTools <unconfigured@null.spigotmc.org>
2019-02-28 23:54:19 -0300

    Trying to fix ios

--- hotel-biss-inn/index.js
+++ hotel-biss-inn/index.js
@@ -147,12 +147,10 @@
   }
 
   // sdfhdklsdfghklsdfgvbklsgbksjjfbhklsdafhklsdfhklsadfhklsadfhklsdafhkljfvbnklsdfvbklwajdhfoçawhfoawrghoeghlçjfgvnl
-  if (!document.body.classList.contains('mobile')) {
     if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
       var info = document.getElementById("infoModal");
       $(info).css("right", "50px")
     }
-  }
 
   // Set handler for scene switch.
   scenes.forEach(function(scene) {
